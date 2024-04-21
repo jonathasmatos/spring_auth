8aaa039f49bdb584be64ae4bc2beaac11ebb846d
Jonathas Matos <5903780@lacen.local>
2024-04-21 09:54:04 -0300

    Criptografando senha em SecurityConfig

--- src/main/java/authentication/authentication/security/SecurityConfig.java
+++ src/main/java/authentication/authentication/security/SecurityConfig.java
@@ -1,27 +1,35 @@
 package authentication.authentication.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("user")
-                .password("{noop}123456") // A senha é "password"
+                .password("$2y$10$CCvJCRoLucSX3vK9RhLd9ee.AlShubSBSfPqExGdG3kI.6QVNcHs2") // Senha encriptografada manualmente
                 .roles("USER");
     }
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-        .authorizeRequests().antMatchers("users/create", "/users/create/**").permitAll().and().httpBasic();
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("users/create", "/users/create/**").permitAll().and().httpBasic();
 
-    }   
+    }
 }
